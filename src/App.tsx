3205c1ccffa7bd4446ba144747ffbafaa4d03c98
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-11-02 16:34:40 -0400

    Change index to scss file, add google fonts, intial header styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Routes from "./Components/Routes/Routes";
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes />
     </div>
   );
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,1 +1,20 @@
-export default {};
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <Link to="/" className="home-link">
+        <h1>METaphor</h1>
+      </Link>
+      <Link to="/about" className="nav-link">
+        <h2>About</h2>
+      </Link>
+      <Link to="/user-gallery" className="nav-link">
+        <h2>User Gallery</h2>
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
